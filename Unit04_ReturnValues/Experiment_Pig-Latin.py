e5a9f6f8c6a82688e790ae99fe81c30070264b6a
jabez-churchill <67837213+jabez-churchill@users.noreply.github.com>
2020-07-09 00:09:02 +0700

    Removed recursion. Removed scaffolding.

--- Unit04_ReturnValues/Experiment_Pig-Latin.py
+++ Unit04_ReturnValues/Experiment_Pig-Latin.py
@@ -1,64 +1,43 @@
 # Pig Latin Translator
-vowel = set(['a', 'e', 'i', 'o', 'u'])
+vowel = set(['a', 'e', 'i', 'o', 'u', 'y'])
+# illegal = set(['.', '!', '?'])
 
 
 def to_piglatin(term, position=0):
-    """Translates a word (as argument) to Pig Latin, using string slices
-    and recursion."""
-    while term[position] not in vowel:
+    """Prints a word (as argument) to Pig Latin, using string slices
+    and returns."""
+
+    while term[position] not in vowel and position < len(term):
         position += 1
     if term[position] in vowel and position == 0:
-        # print(term + 'yay')
         return str(term + 'yay')
     elif term[position] in vowel or position == len(term)-1:
-        # print(term[position:] + term[:position] + 'ay')
         return str(term[position:] + term[:position] + 'ay')
     else:
-        # if not a vowel and not last letter
-        return str(term[position:] + term[position] + 'ay')
-        # position += 1
-        # to_piglatin(term, position)
+        return str('')
 
 
 # Rules for Pig-Latin
 # If vowel, add 'yay' to the end.
-# If consenant (followed by vowel), add 'ay'
-# If consenant group (followed by vowel), add 'ay'
-
-def input_to():
-    q = input('Translate to Pig Latin:  ')
-    to_piglatin(str.lower(q))
-
-
-# input_to()
+# If consenant or group, move behind first vowel and add 'ay'
 
 
 # Get Sentence
 def get_sentence():
+    """User inputted string, converts it to a list. """
+
     sen = str.lower(input('Write a sentence to translate:  '))
     userSentence = sen.split()
-    print(userSentence)
     translate_sen(userSentence)
 
 
 def translate_sen(senlist, pos=0):
+
     output = ''
     while pos < len(senlist):
         output = output + to_piglatin(senlist[pos]) + ' '
         pos += 1
     print(output)
-    # print(output)
 
 
 get_sentence()
-
-# Iterate through sentence and translate words.
-# def run_sentence(word=0):
-    # global userSentence
-    # global userOutput
-    # inp = input("Type a sentence to translate:  ")
-    # userSentence = inp.split()
-    # if word == len(userSentence):
-    #     print(userOutput)
-    # else:
-    #     userOutput = userOutput + to_piglatin(userSentence[word])
