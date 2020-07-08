37c51981fdd6de8b9f8e0f4bb47859284733eb28
jabez-churchill <67837213+jabez-churchill@users.noreply.github.com>
2020-07-08 14:52:42 +0700

    Global variables, string slice

--- Unit04_ReturnValues/Experiment_Pig-Latin.py
+++ Unit04_ReturnValues/Experiment_Pig-Latin.py
@@ -1,29 +1,43 @@
 # Pig Latin Translator
 vowel = set(['a', 'e', 'i', 'o', 'u'])
+userSentence = []
+userOutput = []
 
 
 def to_piglatin(term, position=0):
-    build = ''
-    beginning = ''
+    """Translates a word (as argument) to Pig Latin, using string slices
+    and recursion."""
     if term[position] in vowel and position == 0:
-        return term + 'ay'
-    # finished... found a vowel
-    elif term[position] in vowel:
-        return build + beginning + 'ay'
+        # print(term + 'yay')
+        return term + 'yay'
+    elif term[position] in vowel or position == len(term)-1:
+        # print(term[position:] + term[:position] + 'ay')
+        return term[position:] + term[:position] + 'ay'
     else:
-        print("Not a vowel")
-        build = build + term[position]
-        beginning = beginning + term[position]
         position += 1
         to_piglatin(term, position)
 
-
-to_piglatin('Win')
-to_piglatin('off')
 
 # Rules for Pig-Latin
 # If vowel, add 'yay' to the end.
 # If consenant (followed by vowel), add 'ay'
 # If consenant group (followed by vowel), add 'ay'
 
-# ending is word's
+def input_to():
+    q = input('Translate to Pig Latin:  ')
+    to_piglatin(str.lower(q))
+
+
+input_to()
+
+
+# Iterate through sentence and translate words.
+def run_sentence(word=0):
+    global userSentence
+    global userOutput
+    inp = input("Type a sentence to translate:  ")
+    userSentence = inp.split()
+    if word == len(userSentence):
+        print(userOutput)
+    else:
+        userOutput = userOutput + to_piglatin(userSentence[word])
