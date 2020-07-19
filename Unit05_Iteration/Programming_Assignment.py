dc014d62c9e4ac2fb048f05d8d45bf15e792cfe3
jabez-churchill <67837213+jabez-churchill@users.noreply.github.com>
2020-07-19 15:46:35 +0700

    assignement submission

--- Unit05_Iteration/Programming_Assignment.py
+++ Unit05_Iteration/Programming_Assignment.py
@@ -2,37 +2,26 @@ import math
 
 
 # PART 1
-def my_sqrt(a):
+def my_sqrt(a):  # CRITERIA 1: Include 1 parameter.
     """Function that takes a as a parameter,
     chooses a starting value for x, and returns an
     estimate of the square root of a."""
-    x = 2  # Estimate value for x given default of 2
-    while True:
+    x = 2  # # CRITERIA 2: Initialize x
+    while True:  # CRITERIA 1: Include while loop.
         y = (x + a/x) / 2.0
         if y == x:
             break
-        x = y
+        x = y  # CRITERIA 2: Return final value of x
     return y  # After break, return estimate of square root.
-
-
-# # TEST my function vs. math module
-# print('Part 1:')
-# print(my_sqrt(20))
-# print(math.sqrt(20))
-
-# print(my_sqrt(25))
-# print(math.sqrt(25))
-# print('')
-# print('Part 2:')
 
 
 # PART 2
 def test_sqrt():
     a = 1
-    while a <= 25:
-        mysqrt = my_sqrt(a)
-        mathsqrt = math.sqrt(a)
-        diff = abs(mysqrt - mathsqrt)
+    while a <= 25:  # CRITERIA 3: Print index of 1 - 25
+        mysqrt = my_sqrt(a)  # CRITERIA 4: Print values of my_sqrt
+        mathsqrt = math.sqrt(a)  # CRITERIA 5: Print values of math.sqrt
+        diff = abs(mysqrt - mathsqrt)  # CRITERIA 6: Print values of diff
         print('a =', a, '| my_sqrt(a) = ', mysqrt, '| math.sqrt(a) =', mathsqrt, "| diff =", diff)
         a += 1
 
